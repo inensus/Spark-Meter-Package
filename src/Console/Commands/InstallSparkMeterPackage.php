6ccc1ed080b3be454fafca3ab2c51b6c1e9f22a3
93Kamuran <enginkamuran@gmail.com>
2020-10-30 12:52:02 +0300

    InstallSparkMeterPackage.php refactored.

--- src/Console/Commands/InstallSparkMeterPackage.php
+++ src/Console/Commands/InstallSparkMeterPackage.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Inensus\SparkMeter\Helpers\InsertSparkMeterApi;
 use Inensus\SparkMeter\Services\CredentialService;
 use Inensus\SparkMeter\Services\MeterModelService;
+use Inensus\SparkMeter\Services\MenuItemService;
 
 class InstallSparkMeterPackage extends Command
 {
@@ -15,6 +16,7 @@ class InstallSparkMeterPackage extends Command
     private $insertSparkMeterApi;
     private $meterModelService;
     private $credentialService;
+    private $menuItemService;
 
     /**
      * Create a new command instance.
@@ -22,16 +24,19 @@ class InstallSparkMeterPackage extends Command
      * @param InsertSparkMeterApi $insertSparkMeterApi
      * @param MeterModelService $meterModelService
      * @param CredentialService $credentialService
+     * @param MenuItemService $menuItemService
      */
     public function __construct(
         InsertSparkMeterApi $insertSparkMeterApi,
         MeterModelService $meterModelService,
-        CredentialService $credentialService
+        CredentialService $credentialService,
+        MenuItemService $menuItemService
     ) {
         parent::__construct();
         $this->insertSparkMeterApi = $insertSparkMeterApi;
         $this->meterModelService = $meterModelService;
         $this->credentialService=$credentialService;
+        $this->menuItemService=$menuItemService;
     }
 
     public function handle(): void
@@ -55,6 +60,22 @@ class InstallSparkMeterPackage extends Command
 
         $this->insertSparkMeterApi->registerSparkMeterManufacturer();
         $this->credentialService->createSmCredentials();
+
+        $this->call('plugin:add', [
+            'name' => "SparkMeter",
+            'composer_name' => "inensus/spark-meter",
+            'description' => "Spark meters integration package for MicroPowerManager",
+        ]);
+        $this->call('routes:generate');
+
+        $menuItems = $this->menuItemService->createMenuItems();
+        $this->call('menu-items:generate', [
+            'menuItem' => $menuItems['menuItem'],
+            'subMenuItems' => $menuItems['subMenuItems'],
+        ]);
+
+        $this->call('sidebar:generate');
+
         $this->info('Package installed successfully..');
     }
 }
