44bb2b8d7e6599c8aa2b57575d38cb48d06cbac9
kamuran engin <43153928+93Kamuran@users.noreply.github.com>
2020-10-30 18:02:40 +0300

    Update MenuItemService.php

--- src/Services/MenuItemService.php
+++ src/Services/MenuItemService.php
@@ -16,23 +16,23 @@ class MenuItemService
 
         $subMenuItem1=[
             'name' =>'Overview',
-            'url_slug' =>'spark-meters/sm-overview',
+            'url_slug' =>'/spark-meters/sm-overview',
         ];
         array_push($subMenuItems, $subMenuItem1);
         $subMenuItem2=[
             'name' =>'Meter Models',
-            'url_slug' =>'spark-meters/sm-meter-model/page/1',
+            'url_slug' =>'/spark-meters/sm-meter-model/page/1',
         ];
         array_push($subMenuItems, $subMenuItem2);
         $subMenuItem3=[
             'name' =>'Tariffs',
-            'url_slug' =>'spark-meters/sm-tariff/page/1',
+            'url_slug' =>'/spark-meters/sm-tariff/page/1',
         ];
         array_push($subMenuItems, $subMenuItem3);
      
         $subMenuItem4=[
             'name' =>'Customers',
-            'url_slug' =>'spark-meters/sm-customer/page/1',
+            'url_slug' =>'/spark-meters/sm-customer/page/1',
         ];
         array_push($subMenuItems, $subMenuItem4);
 
