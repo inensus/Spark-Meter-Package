95c2f8e91f45baf21063ebe2c9bc8a19942527bb
93Kamuran <enginkamuran@gmail.com>
2020-10-30 12:51:15 +0300

    MenuItemService.php added for import menu items to database.

---
+++ src/Services/MenuItemService.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Inensus\SparkMeter\Services;
+
+class MenuItemService
+{
+    public function createMenuItems()
+    {
+        $menuItem = [
+            'name' =>'Spark Meter',
+            'url_slug' =>'',
+            'md_icon' =>'bolt'
+        ];
+        $subMenuItems= array();
+
+        $subMenuItem1=[
+            'name' =>'Overview',
+            'url_slug' =>'spark-meters/sm-overview',
+        ];
+        array_push($subMenuItems, $subMenuItem1);
+        $subMenuItem2=[
+            'name' =>'Meter Model',
+            'url_slug' =>'spark-meters/sm-meter-model/page/1',
+        ];
+        array_push($subMenuItems, $subMenuItem2);
+        $subMenuItem3=[
+            'name' =>'Tariffs',
+            'url_slug' =>'spark-meters/sm-tariff/page/1',
+        ];
+        array_push($subMenuItems, $subMenuItem3);
+        $subMenuItem4=[
+            'name' =>'Customers',
+            'url_slug' =>'spark-meters/sm-overview',
+        ];
+        array_push($subMenuItems, $subMenuItem4);
+        $subMenuItem1=[
+            'name' =>'Overview',
+            'url_slug' =>'spark-meters/sm-customer/page/1',
+        ];
+        array_push($subMenuItems, $subMenuItem1);
+
+        return ['menuItem'=>$menuItem,'subMenuItems'=>$subMenuItems];
+
+
+    }
+}
