75b7072ef668f5974a552511bf13ea438be57e15
93Kamuran <enginkamuran@gmail.com>
2021-04-07 10:55:29 +0300

    New sms types and related sources added to SparkSmsConfig.php.

--- src/Sms/Senders/SparkSmsConfig.php
+++ src/Sms/Senders/SparkSmsConfig.php
@@ -9,6 +9,8 @@ class SparkSmsConfig extends SmsConfigsCore
 {
     public $smsTypes = [
         SparkSmsTypes::LOW_BALANCE_LIMIT_NOTIFIER => 'Inensus\SparkMeter\Sms\Senders\LowBalanceLimitNotifier',
+        SparkSmsTypes::BALANCE_FEEDBACK => 'Inensus\SparkMeter\Sms\Senders\BalanceFeedback',
+        SparkSmsTypes::METER_RESET_FEEDBACK => 'Inensus\SparkMeter\Sms\Senders\MeterResetFeedback',
     ];
     public $bodyParsersPath = 'Inensus\\SparkMeter\\Sms\\BodyParsers\\';
     public $servicePath = 'Inensus\SparkMeter\Services\SmSmsBodyService';
